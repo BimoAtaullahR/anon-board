f50dc07fa732eee69d64cc84b5f502b67942cf4d
bimo <bimoataullahrahman38@gmail.com>
2025-10-25 21:48:58 +0700

    masih bingung cara agar data api nya terupdate

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -5,8 +5,9 @@ type Pesan = {
   time: Date;
   id: number;
 };
+let id = 4;
 
-const pesann: Pesan[] = [
+let pesann: Pesan[] = [
     { message: "ini pesan pertama", time: new Date(), id: 1 },
     { message: "ini pesan kedua", time: new Date(), id: 2 },
     { message: "ini pesan ketiga", time: new Date(), id: 3 },
@@ -15,5 +16,10 @@ const pesann: Pesan[] = [
 export default function handler(req: NextApiRequest, res: NextApiResponse <Pesan[]>){
   if(req.method==="GET"){
     res.status(200).json(pesann);
+  }
+  else if(req.method==="POST"){
+    pesann = [{message: JSON.parse(req.body), time: new Date(), id: id}, ...pesann];
+    id++;
+    res.status(200);
   }
 }
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -2,16 +2,23 @@ import React from 'react'
 import { useState } from 'react'
 
 export async function buatPesan(pesan: string){
-    const res
+  const res = await fetch('http://localhost:3000/api/posts', {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(pesan),
+  })
+  
 }
 
 function createMessage() {
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <div>
         <p>Silakan isi form nya</p>
-        <textarea value = {message} onChange={(e) => setMessage(e.target.value)} ></textarea>
-        <button className='btn btn-' onSubmit={(e) => buatPesan(e.target.value)}>Submit</button>
+        <form onSubmit={(e) => {e.preventDefault(); buatPesan(message)}}>
+          <input type='text' value={message} onChange={(e) => setMessage(e.target.value)}/>
+          <button type='submit'>Submit</button>
+        </form>
     </div>
   )
 }
