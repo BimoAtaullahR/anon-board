38fcb218c44c2678959e3e23d74c6e2804a3b802
bimo <bimoataullahrahman38@gmail.com>
2025-10-30 22:23:05 +0700

    ngedit dikit tok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function index() {
   return (
-    <div className='flex flex-col'>index
+    <div className='flex flex-col bg-amber-400 w-100'>index
         <Link href='/login'>Login</Link>
         <Link href='/anonboard'>anonboard</Link>
 
